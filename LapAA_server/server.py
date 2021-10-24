764afe215692094693c32dbfdab155bf94da24a9
Artem <ar.kashapov@gmail.com>
2021-10-25 00:55:34 +0400

    Attempt to get process files

--- LapAA_server/server.py
+++ LapAA_server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, send_file
+from flask.scaffold import F
 from laplacian import Laplacian
 from PIL import Image
 import numpy as np
@@ -13,11 +14,13 @@ def process():
     # if request.method == 'POST':
     if 'file' in request.files:
         file = request.files['file']
-        print('File получен')
+        # print(file[0])
 
-    with open('source\\auto.jpg', "rb") as image:
-        b = bytearray(image.read())
-    
+    try:
+        with open(file, "rb") as image:
+            b = bytearray(image.read())
+    except:
+        print('Не тут то было')
     out = Laplacian(b).run()
     
     Image.fromarray(np.uint8(out) , 'L').save("source\output.jpeg")
